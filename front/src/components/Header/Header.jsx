916c8445da6dd683401e0a30ad308e3cdfbf005a
unknown <andjeybur@gmail.com>
2022-12-16 02:40:18 +0300

    timer fix iphone problem

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -7,14 +7,30 @@ import TimerSnow from '../../img/TimerSnow.svg'
 import store from "../../store/store";
 import {observer} from "mobx-react-lite";
 import Timer from "./Timer";
+import Music from "../../audio/AngelsOfLight.mp3";
+
+const onSetMusic = () => store.setMusic(Music)
 
 const Header = () => {
+    // const ref = React.useRef(null);
+
+    // React.useEffect(() => {
+        // ref.current.play()
+        // ref.current.addEventListener('ended', onSetMusic);
+        // return () => {
+        //     ref.current.removeEventListener('ended', onSetMusic);
+        // };
+    // }, [])
+
     const toggleIsSound = () => {
+        // console.log(ref.current)
         store.setToggleIsSound()
+        // store.isSound ? ref.current.play() : ref.current.pause()
     }
 
     return (
         <header className="header">
+            {/*<audio src={Music} ref={ref} muted={true}/>*/}
             <div className="container">
                 <div className="header__row">
                     {window.innerWidth > 1140 ? <div className="header__top">
@@ -58,7 +74,7 @@ const Header = () => {
                                 <img src={TimerSnow} alt="Снежок" className="timer__snow"/> : ''}
                             До нового года осталось:
                         </div>
-                        <Timer />
+                        <Timer/>
                     </div>
                 </div>
             </div>
