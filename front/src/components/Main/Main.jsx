2fd703de1c6a3fd4d7088da8710525661109b43d
unknown <andjeybur@gmail.com>
2022-12-16 09:51:04 +0300

    add mobile static to main page

--- front/src/components/Main/Main.jsx
+++ front/src/components/Main/Main.jsx
@@ -6,35 +6,37 @@ import store from "../../store/store";
 import Game from "../Game/Game";
 import MainPage from "./MainPage";
 import Rules from "./Rules";
-import HowToPlay from "./How2Play";
+import HowToPlay from "./HowToPlay";
 import Meaning from "./Meaning";
 import Feedback from "./Feedback";
 
 const Main = () => {
     return (
         <main className="main">
-            <div className="container">
-                <div className="main__content">
-                    <Routes>
-                        <Route path="/" element={
-                            <div className="main-page">
+            <div className="main__content">
+                <Routes>
+                    <Route path="/" element={
+                        <div className="main-page">
+                            <div className="container">
                                 <MainPage/>
                                 <Rules/>
                             </div>
-                        }/>
-                        <Route path="/login" element={<Login userData={store.userData}/>}/>
-                        <Route path="/register" element={<Register userData={store.userData}/>}/>
-                        <Route path="/game" element={
-                            <div className="main-page">
+                        </div>
+                    }/>
+                    <Route path="/login" element={<Login userData={store.userData}/>}/>
+                    <Route path="/register" element={<Register userData={store.userData}/>}/>
+                    <Route path="/game" element={
+                        <div className="main-page">
+                            <div className="container">
                                 <Game/>
                                 <Meaning/>
                                 <Rules/>
                                 <HowToPlay/>
-                                <Feedback/>
                             </div>
-                        }/>
-                    </Routes>
-                </div>
+                            <Feedback/>
+                        </div>
+                    }/>
+                </Routes>
             </div>
         </main>
     );
