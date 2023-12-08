a47ecaf8080aac5a6567a8181a1f2178086e08e0
MegaVerkruzo <robochat@mail.ru>
2023-12-08 20:39:46 +0300

    Add migrations

--- backend/src/main/resources/db/changelog/0.0.1/changelog-3.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset GrunskiiAleksei:5
---comment update id column in table answers
-ALTER TABLE answers
-    ALTER COLUMN id TYPE BIGINT
---rollback DROP TABLE users
--- backend/src/main/resources/db/changelog/0.0.1/changelog-4.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
-
---changeset GrunskiiAleksei:5
---comment update id column in table attempts
-ALTER TABLE attempts
-    ALTER COLUMN id TYPE BIGINT
---rollback DROP TABLE users
