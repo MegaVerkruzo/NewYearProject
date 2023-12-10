0b6177e043177304e93ec9407dcbb609982b2826
MegaVerkruzo <robochat@mail.ru>
2023-12-10 15:21:51 +0300

    Move on from javax to jakarta

--- backend/src/main/java/com/commercial/backend/model/Answer.java
+++ backend/src/main/java/com/commercial/backend/model/Answer.java
@@ -1,8 +1,11 @@
 package com.commercial.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-import javax.persistence.*;
 import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 
