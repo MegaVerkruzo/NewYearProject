8b15c1880daa4f85567f90dc68b0333670feaee8
MegaVerkruzo <robochat@mail.ru>
2023-12-10 14:25:21 +0300

    Add ConfigRepository

---
+++ backend/src/main/java/com/commercial/backend/db/entities/ConfigField.java
@@ -0,0 +1,47 @@
+package com.commercial.backend.db.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class ConfigField {
+    @Id
+    @Column(name = "id_name")
+    private String nameId;
+    @Column(name = "str_property")
+    private String stringProperty;
+    @Column(name = "date_property")
+    private LocalDateTime dateProperty;
+    @Column(name = "long_property")
+    private Long longProperty;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ConfigField that = (ConfigField) o;
+        return getNameId() != null && Objects.equals(getNameId(), that.getNameId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
