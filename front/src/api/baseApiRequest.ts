e638bc5d4638692f3e634998ad500c884b609e46
Andrey Burakov <andjeybur@gmail.com>
2023-12-13 20:22:16 +0300

    fix: unused vars

--- front/src/api/baseApiRequest.ts
+++ front/src/api/baseApiRequest.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { API_URL } from '../config/config'
-import { getTgParams } from '../utils/gettgParams'
+import { getTgParams } from '../utils/getTgParams'
 
 interface BaseApiRequestOptions {
   url: string
--- front/src/assets/svgs/RuleIcon3.tsx
+++ front/src/assets/svgs/RuleIcon3.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const RuleIcon3 = () => {
   return (
     <svg
