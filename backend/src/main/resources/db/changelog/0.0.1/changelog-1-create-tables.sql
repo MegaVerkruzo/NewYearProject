6cbcbed23cfa64943ba31694cc224618b6e09baa
MegaVerkruzo <robochat@mail.ru>
2023-12-14 08:43:37 +0300

    Push changes

--- backend/src/main/resources/db/changelog/0.0.1/changelog-1-create-tables.sql
+++ backend/src/main/resources/db/changelog/0.0.1/changelog-1-create-tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users
     middle_name VARCHAR(255)                                     NOT NULL,
     email       VARCHAR(255)                                     NOT NULL,
     place       VARCHAR(255)                                     NOT NULL,
-    token       VARCHAR(255)                                     NOT NULL UNIQUE,
+    token       VARCHAR(255)                                     NOT NULL UNIQUE
 );
 --rollback DROP TABLE users
 
