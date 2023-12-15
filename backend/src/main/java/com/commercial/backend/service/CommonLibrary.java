a27268c9f486e4a41df7820b26d34617dd0e870a
MegaVerkruzo <robochat@mail.ru>
2023-12-15 08:41:08 +0300

    Make new Feedback table

--- backend/src/main/java/com/commercial/backend/service/CommonLibrary.java
+++ backend/src/main/java/com/commercial/backend/service/CommonLibrary.java
@@ -8,18 +8,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.function.Function;
 
 public class CommonLibrary {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public static Long parseId(String token) throws NotRegisteredException, NotValidException {
+        return parseSomething(token, JsonUser::getId);
+    }
+
+    public static String parseUsername(String token) throws NotRegisteredException, NotValidException {
+        return parseSomething(token, JsonUser::getUsername);
+    }
+
+    public static <T> T parseSomething(
+            String token,
+            Function<JsonUser, T> func
+    ) throws NotRegisteredException, NotValidException {
         if (token == null) {
             throw new NotValidException();
         }
         // :TODO add exception handling
         String userJson = URLDecoder.decode(token, StandardCharsets.UTF_8).split("&")[1].split("=")[1];
-        ObjectMapper mapper = new ObjectMapper();
-        Long result;
+        T result;
         try {
-            result = mapper.readValue(userJson, JsonUser.class).getId();
+            result = func.apply(mapper.readValue(userJson, JsonUser.class));
         } catch (JsonProcessingException e) {
             throw new NotValidException();
         }
