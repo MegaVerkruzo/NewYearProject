13065c1db1af7f4f77c1063247083bb3a89e7d57
Andrey Burakov <andjeybur@gmail.com>
2023-12-17 20:34:25 +0300

    fix: add eruda

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,5 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import { Router } from './components/Router/Router'
 import './styles/index.css'
+import eruda from 'eruda'
+
+const el = document.createElement('div')
+document.body.appendChild(el)
+
+eruda.init({
+  container: el,
+  tool: ['console', 'elements'],
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Router />)
