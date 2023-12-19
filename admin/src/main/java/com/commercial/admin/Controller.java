1bcba196bff34f1da790d28be0e8b13950b9661d
MegaVerkruzo <robochat@mail.ru>
2023-12-19 21:16:03 +0300

    Fix small bags

--- admin/src/main/java/com/commercial/admin/Controller.java
+++ admin/src/main/java/com/commercial/admin/Controller.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @org.springframework.stereotype.Controller
 @AllArgsConstructor
+@RequestMapping("admin")
 public class Controller {
     private final Logger logger = LoggerFactory.getLogger(Controller.class);
     private final UserRepository userRepository;
