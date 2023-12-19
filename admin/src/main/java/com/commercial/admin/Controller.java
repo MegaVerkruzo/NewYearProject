adc2cd3829df3c03060941416cf263e34742565b
MegaVerkruzo <robochat@mail.ru>
2023-12-19 03:25:25 +0300

    Update admin

--- admin/src/main/java/com/commercial/admin/Controller.java
+++ admin/src/main/java/com/commercial/admin/Controller.java
@@ -2,13 +2,17 @@ package com.commercial.admin;
 
 import com.commercial.admin.db.UserRepository;
 import com.commercial.admin.db.entities.User;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
@@ -16,6 +20,7 @@ import java.util.List;
 public class Controller {
     private final Logger logger = LoggerFactory.getLogger(Controller.class);
     private final UserRepository userRepository;
+    private final ExcelService excelService;
 
     @GetMapping("/")
     public String method(Model model) {
@@ -25,5 +30,23 @@ public class Controller {
 
         model.addAttribute("userList", result);
         return "hello";
+    }
+
+    @GetMapping("/users")
+    public void reportUsersExcel(HttpServletResponse response) throws IOException {
+        XSSFWorkbook workbook = excelService.getWorkBook();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            workbook.write(bos);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            bos.close();
+        }
+        response.getOutputStream().write(bos.toByteArray());
+
+        response.setContentType("application/x-download");
+        response.setHeader("Content-Disposition", "attachment; filename=" + java.net.URLEncoder.encode("XXXX.xlsx", StandardCharsets.UTF_8));
+        workbook.write(response.getOutputStream());
     }
 }
